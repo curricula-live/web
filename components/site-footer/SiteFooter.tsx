a7423301e95f21ce8abb3f4d091ddbff04f25d7e
Dragoș-Andrei Bobu <dragosandreibobu@gmail.com>
2026-09-10 20:40:55 +0300

    web: add minimal site footer

---
+++ components/site-footer/SiteFooter.tsx
@@ -0,0 +1,10 @@
+import styles from "./SiteFooter.module.css";
+
+export function SiteFooter() {
+  return (
+    <footer className={styles.footer}>
+      <span>Privacy</span>
+      <span>Contact</span>
+    </footer>
+  );
+}
